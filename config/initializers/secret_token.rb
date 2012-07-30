b9b4d0981de8529c8f10f6141472b1d2c396342b
Vik Kumar <vik.ceo@gmail.com>
2012-07-30 15:56:02 -0700

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SakshumApp::Application.config.secret_token = 'e6b7d9e92af46692c85483b6ebdfd8928921ac96131fdeab6c6b07c4554d90f6d69de51dfe5859e4c806160cfb758e5e5d136db3625fa24a8ea4ce6c40800b26'
